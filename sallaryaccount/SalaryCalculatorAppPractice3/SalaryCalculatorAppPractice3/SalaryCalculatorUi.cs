098b2a95d0969f70eb3cc95a5317a78622cbc55e
Mohammad Fazle Karim <mohammadfaglekarim@gmail.com>
2020-01-31 19:24:59 +0600

    Account_Operation_Added

---
+++ sallaryaccount/SalaryCalculatorAppPractice3/SalaryCalculatorAppPractice3/SalaryCalculatorUi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorAppPractice3
+{
+    public partial class SalaryCalculatorUi : Form
+    {
+        public SalaryCalculatorUi()
+        {
+            InitializeComponent();
+        }
+
+        Salary salary = new Salary();
+
+        private void ShowMeSalaryButton_Click(object sender, EventArgs e)
+        {
+           double bs = Convert.ToDouble(basicAmountTextBox.Text);
+           double hr=Convert.ToDouble(homeRentTextBox.Text);
+           double md= Convert.ToDouble(medicalAllowanceTextBox.Text);
+
+            salary.employeeName = employeeNameTextBox.Text;
+            MessageBox.Show(salary.Calculate(bs, hr, md));
+
+
+
+
+        }
+    }
+}
